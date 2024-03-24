a9208d26aaccc18683de187eedd23c36dfcf9775
Adriano Miranda <adrjmiranda@gmail.com>
2024-03-24 20:34:32 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { FunctionComponent, ReactNode } from 'react';
+
+const App: FunctionComponent = (): ReactNode => {
+	return (
+		<div>
+			<h1>App</h1>
+		</div>
+	);
+};
+
+export default App;
